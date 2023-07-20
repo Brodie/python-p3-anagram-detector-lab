9a9aed00256538075aea27a90e0b60d3b11f1c78
Brodie Ashcraft <brodiea19@gmail.com>
2023-07-20 11:37:17 -0700

    anagrams detected!!!

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        sol = []
+        for ana in list:
+            if sorted(self.word) == sorted(ana):
+                sol.append(ana)
+        return sol
